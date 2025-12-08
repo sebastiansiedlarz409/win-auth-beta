68608da0913397d1de43ce03c5ebde7ce4daeb55
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2025-12-08 17:25:16 +0100

    Semaphore instead of lock in session storage

--- WinAuth.Example/Auth/WinAuthSessionMemoryStorage.cs
+++ WinAuth.Example/Auth/WinAuthSessionMemoryStorage.cs
@@ -5,7 +5,7 @@ namespace WinAuth.Example.Auth
 {
     public sealed class WinAuthSessionMemoryStorage : IWinAuthSessionStorage
     {
-        private readonly object _lock = new object();
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
         private List<WinAuthSession> _sessions = new List<WinAuthSession>();
 
         /// <summary>
@@ -16,10 +16,9 @@ namespace WinAuth.Example.Auth
         /// <exception cref="NotImplementedException"></exception>
         private async Task CleanupSession()
         {
-            lock (_lock)
-            {
-                _sessions.RemoveAll(t=>t.ExpirationDate<DateTime.UtcNow);
-            }
+            await _semaphore.WaitAsync();
+            _sessions.RemoveAll(t => t.ExpirationDate < DateTime.UtcNow);
+            _semaphore.Release();
         }
 
         public async Task<WinAuthSession?> GetSessionAsync(Guid sessionId)
@@ -27,41 +26,41 @@ namespace WinAuth.Example.Auth
             //remove expired sessions
             await CleanupSession();
 
-            WinAuthSession? session = null;
-            lock (_lock)
-            {
-                session = _sessions.FirstOrDefault(t => t.SessionId == sessionId);
-            }
+            await _semaphore.WaitAsync();
+
+            WinAuthSession? session = _sessions.FirstOrDefault(t => t.SessionId == sessionId);
+            _semaphore.Release();
+
             return session;
         }
 
         public async Task UpdateSessionAsync(WinAuthSession session)
         {
-            lock (_lock)
+            await _semaphore.WaitAsync();
+
+            var updateSession = _sessions.FirstOrDefault(t => t.SessionId == session.SessionId);
+            if (updateSession is { })
             {
-                var updateSession = _sessions.FirstOrDefault(t => t.SessionId == session.SessionId);
-                if(updateSession is { })
-                {
-                    updateSession.ExpirationDate = session.ExpirationDate;
-                    updateSession.UserName = session.UserName;
-                }
+                updateSession.ExpirationDate = session.ExpirationDate;
+                updateSession.UserName = session.UserName;
             }
+            _semaphore.Release();
         }
 
         public async Task RemoveSessionAsync(WinAuthSession session)
         {
-            lock (_lock)
-            {
-                _sessions.Remove(session);
-            }
+            await _semaphore.WaitAsync();
+
+            _sessions.Remove(session);
+            _semaphore.Release();
         }
 
         public async Task StoreSessionAsync(WinAuthSession session)
         {
-            lock(_lock)
-            {
-                _sessions.Add(session);
-            }
+            await _semaphore.WaitAsync();
+
+            _sessions.Add(session);
+            _semaphore.Release();
         }
     }
 }
